f8ee8d71b8718c15896179ee2b10afcfdded2044
dito22s <132772529+dito22s@users.noreply.github.com>
2023-06-26 23:16:07 +0400

    3 files modified, 7 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# frontend-final
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-e4t84u)
--- main.js
+++ main.js
@@ -1,24 +1,92 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+var httpRequest = new XMLHttpRequest();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+httpRequest.onreadystatechange = processResponse;
+httpRequest.open("GET", 'https://jsonplaceholder.typicode.com/users');
+httpRequest.send();
 
-setupCounter(document.querySelector('#counter'))
+function processResponse() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var jsonData = JSON.parse(httpRequest.responseText);
+      console.log(jsonData);
+    }
+  }
+}
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    const userElements = data.slice(0, 6).map((user, index) => {
+      const { name, id, email, phone } = user;
+      const userElement = document.createElement('div');
+      userElement.innerHTML = `<h2 class="nebiName"><u>${name}</u></h2>
+                               <div class="nebiDiv">
+                                 <h5 class="nebi">ID: ${id}</h5>
+                                 <h5 class="nebi">Email: ${email}</h5>
+                                 <h5 class="nebi">Phone: ${phone}</h5>
+                               </div>`;
+      return userElement;
+    });
+
+    const logButtons = document.querySelectorAll('.log-button');
+    const inp1 = document.querySelector('.inp1');
+
+    logButtons.forEach((button, index) => {
+      button.addEventListener('click', function() {
+        inp1.value = data[index].id;
+      });
+    });
+
+    const userDivs = document.querySelectorAll('.user-div');
+    userDivs.forEach((div, index) => {
+      div.appendChild(userElements[index]);
+    });
+  })
+  .catch(error => console.log(error));
+
+
+function processResponse() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var jsonData = JSON.parse(httpRequest.responseText);
+      console.log(jsonData);
+
+      fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(photos => {
+          var container = document.getElementById('photoId');
+
+          for (var i = 0; i < 6 && i < photos.length; i++) {
+            var photo = photos[i];
+
+            var img = document.createElement('img');
+            img.src = photo.url;
+            img.alt = photo.title;
+            img.style.width = '220px';
+
+            container.appendChild(img);
+          }
+        })
+        .catch(error => console.log(error));
+    }
+  }
+}
+
+
+async function addpost(form){
+    const title = form.title.value;
+    const body = form.body.value;
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            body: body
+        })
+    })
+}
